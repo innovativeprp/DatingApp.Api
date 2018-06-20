6b9860706db6a701fdfcacb85e5ec57ce5fe989c
Praveen Raveendran <Praveen.Raveendran@hotmail.com>
2018-06-20 12:19:30 +0530

    Implemented Registration Endpoint with DTOs

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using DatingApp.Api.Dtos;
+using DatingApp.API.Data;
+using DatingApp.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController:Controller
+    {
+         private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+     [HttpPost("register")]
+     public async Task<IActionResult> Register([FromBody]UserRegisterDto userRegisterDto)
+     {
+         //validate user
+         userRegisterDto.Username=userRegisterDto.Username.ToLower();
+         if(await _repo.UserExists(userRegisterDto.Username)) return BadRequest("Username is already taken");
+
+        
+         var user =new User { UserName=userRegisterDto.Username};
+
+         var createdUser = await _repo.Register(user,userRegisterDto.Password);
+         return StatusCode(201);
+     }
+       
+    }
+}
